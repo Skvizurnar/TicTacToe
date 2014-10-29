8143819168c09da77cc910f487467c7ca9f096d5
Alein <alexandraeinars@gmail.com>
2014-10-29 15:53:48 +0000

    Various changes, mostly names

--- src/test/java/is/ru/verk/TestTicTacToe.java
+++ src/test/java/is/ru/verk/TestTicTacToe.java
@@ -3,23 +3,11 @@ package is.ru.verk;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class TestBoard {
+public class TestTicTacToe {
 
         public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.verk.testPlayer");
     }
-	
-	@Test
-	public void testEmptyBoard(){
-	
-	Board testBoard = new Board();
-	
-		for(int i = 0; i < 3; i++)	{
-			for(int j = 0; j < 3; j++)	{
-				assertEquals(' ', testBoard.board[i][j]);
-		}
-	}
         
-}
 }
 
