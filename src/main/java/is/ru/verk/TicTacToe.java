80531771d135adceb64675a1e9fa1759704d69d9
Alein <alexandraeinars@gmail.com>
2014-10-29 20:41:49 +0000

    Main added, not fully completed

--- src/main/java/is/ru/verk/TicTacToe.java
+++ src/main/java/is/ru/verk/TicTacToe.java
@@ -1,14 +1,50 @@
 package is.ru.verk;
 import java.util.*;
-
+import java.io.Console;
  
 public class TicTacToe {
 
 	public static void main(String[] args)
     	{
 		Board board = new Board();
+		Player P1 = new HumanPlayer('X');
+		Player P2 = new ComputerPlayer();
+		Bool win = false;
+		Console console = System.console();
+
 		board.setBoard();
+
+		board.printBoard();
+
+		currentPlayer = Player1;
+
+		while(win == false)	{
+
+		System.out.println("It is: " + currPlayer.getName() +  + "'s move..");
+		currentPlayer.makeAMove();
+
+		board.printBoard();
+
+		if(board.hasWon(currentPlayer.getSymbol()))	{
+			win = true;
+		}
+		else if(board.isBoardFull())	{
+			
+			System.out.println("The game ended in a draw...");
+			break;
+		}		
 		
+		}
+		if(Player2 == currentPlayer)
+		{
+			currentPlayer = Player1;
+		}
+		else
+		{
+			currentPlayer = Player2;
+		}		
+
+
 	}
 
         public static int add(String text){
